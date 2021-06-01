681447bf14c2333b1788646df3c69a625882968a
Desmond Silveira <desmond.silveira@ringdna.com>
2021-06-01 09:39:06 -0700

    Added license and changed style to comply with Google style guide.

--- scripts/vote.js
+++ scripts/vote.js
@@ -1,15 +1,21 @@
+/**
+ * Copyright © 2018 Desmond Silveira.  All rights reserved.
+ *
+ * This software is free to use and extend to all registered members of the
+ * American Solidarity Party.  Any extensions must give credit to the author
+ * and preserve this notice.
+ */
+
 $(document).ready(function () {
-  $("#blt").change(function (event) {
+  $('#blt').change(function (event) {
     if (this.files && this.files[0]) {
-      var reader = new FileReader();
-      var text;
+      const reader = new FileReader();
+      let text;
       reader.onload = function () {
         text = reader.result;
-        $("#blt-content").html("<pre>" + text + "</pre>");
+        $('#blt-content').html('<pre>' + text + '</pre>');
       };
       reader.readAsText(this.files[0]);
-
-      
     }
   });
 });
